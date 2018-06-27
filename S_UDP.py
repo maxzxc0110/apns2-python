2ee555401aa6a31f95be30b3b2566ce560e90354
Max <273206894@qq.com>
2018-06-27 17:19:37 +0800

    Update S_UDP.py

--- S_UDP.py
+++ S_UDP.py
@@ -15,7 +15,7 @@ def make_response(r):
     return code,info
 
 def writeLog(info):
-    logging.basicConfig(filename='/path/log.log',level=logging.WARNING)
+    logging.basicConfig(filename='/home/maxwe86/Desktop/apns/log.log',level=logging.INFO)
     logging.info('[Time]:'+time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())+':'+info)
 
 def micro():
@@ -23,7 +23,6 @@ def micro():
 
 
 def sendApns2(cert,token,payload,headers,conn):
-    # conn = HTTP20Connection('api.push.apple.com:443', ssl_context=tls.init_context(cert='/path/'+cert),cert_password=123456)
     conn.request('POST', '/3/device/%s' % token, body=json.dumps(payload), headers=headers)
     resp = conn.get_response()
     return make_response(resp)
@@ -69,25 +68,31 @@ def getPayloadAndHeaders(message,sound,remark,topic):
     return payload,headers
 
 
+
+
+
 def main():
-    d = {}
     Lport = 9090
     host = "0.0.0.0"
     s = socket.socket(socket.AF_INET,socket.SOCK_DGRAM) 
     s.bind((host,Lport))
     print 'bind to the ' ,host ,Lport
+    d = {}
     while True:
+        
         writeLog('PUSH START~~~~~~~~~~~~~~~~')
         data,addr = s.recvfrom(1024) 
-        writeLog("param="+data)
+
+        
         start = micro()
+        writeLog("param="+data)
         if getParms(data) is True:
             continue
         else:
             ParamErr,sound,cert,remark,token,topic,message = getParms(data)
 
 
-        isfile = os.path.isfile('/path/'+cert)
+        isfile = os.path.isfile('/home/maxwe86/Desktop/apns/'+cert)
         if isfile is False:
             s.sendto('400|{"reason":"certNotExit"}|0.0',addr)   #证书不存在
             writeLog('400|{"reason":"certNotExit"}|0.0')
@@ -97,17 +102,26 @@ def main():
         payload,headers = getPayloadAndHeaders(message,sound,remark,topic)        
         connName = cert.replace('.pem','')
 
-        if connName in d:
-            d[connName].request('POST', '/3/device/%s' % token, body=json.dumps(payload), headers=headers)
-            resp = d[connName].get_response()
-            status,data =  make_response(resp)
-        else:
-            d[connName] = HTTP20Connection('api.push.apple.com:443', ssl_context=tls.init_context(cert='/path/'+cert),cert_password=123456)
-            conn = d[connName]
-            status,data = sendApns2(cert,token,payload,headers,conn)
+        try:
+            if connName in d:
+                writeLog("复用链接:"+connName)
+                d[connName].request('POST', '/3/device/%s' % token, body=json.dumps(payload), headers=headers)
+                resp = d[connName].get_response()
+                status,data =  make_response(resp)
+            else:
+                writeLog("开启一个新链接")
+                d[connName] = HTTP20Connection('api.push.apple.com:443', ssl_context=tls.init_context(cert='/home/maxwe86/Desktop/apns/'+cert),cert_password=123456)
+                conn = d[connName]
+                status,data = sendApns2(cert,token,payload,headers,conn)
+            if data=='':
+                data = 'sucess'
+        except:
+            del d[connName]
+            writeLog("清空链接:"+connName)
+            status = 500
+            data = "apns2 connect error"
+            s.close
 
-        if data=='':
-            data = 'sucess'
 
         elapsed = (micro() - start)
         writeLog(str(status)+'|'+data+'|'+str(elapsed))
@@ -118,5 +132,5 @@ def main():
 
 
 if __name__ == '__main__':
-    # sys.stderr = open('/home/maxwe86/Desktop/apns/error.log', 'a+')  #把py错误日志输出到error.log
+    sys.stderr = open('/home/maxwe86/Desktop/apns/error.log', 'a+')  #把py错误日志输出到error.log
     main()
